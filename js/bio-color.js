d80a376d4adcc04cac2bcb6dc4f6ff3f49785728
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-07 13:33:35 +0200

    style: clean code bio-color

--- js/bio-color.js
+++ js/bio-color.js
@@ -1,13 +1,10 @@
 let boxArr = document.querySelectorAll(".bio__grid-item");
 let myPhoto = document.querySelector(".bio__grid-item--noPadding");
-console.log(boxArr);
 
 function colorBox() {
   for (let i = 0; i < boxArr.length; i++) {
     boxArr[i].style.background = "rgba(193, 47, 105, " + Math.random() + ")";
   }
 }
-
-// colorBox();
 
 myPhoto.addEventListener("mouseover", colorBox, false);
