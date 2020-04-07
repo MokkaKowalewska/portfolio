d458e51f4d1829b9d490d00925a22a2f5bb33164
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-07 10:46:24 +0200

    styles: change start background to triangles

--- js/animate-on-scroll.js
+++ js/animate-on-scroll.js
@@ -31,4 +31,14 @@ function showSection() {
   });
 }
 
-document.addEventListener("scroll", showSection, false);
+document.addEventListener("scroll", throttle(showSection, 300), false);
+
+function throttle(fn, wait) {
+  let time = Date.now();
+  return function () {
+    if (time + wait - Date.now() < 0) {
+      fn();
+      time = Date.now();
+    }
+  };
+}
