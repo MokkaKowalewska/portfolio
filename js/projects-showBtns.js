4574099a0c9863795095c2b3779ca807446d7757
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-13 01:45:50 +0200

    feat: #projects add JS showBtns and CSS accordingly

--- js/projects-showBtns.js
+++ js/projects-showBtns.js
@@ -1,2 +1,18 @@
 let btns = document.querySelector(".projects__btns"),
   img = document.querySelector(".projects__img");
+
+img.addEventListener(
+  "mouseover",
+  function (e) {
+    btns.classList.add("btnsSlide");
+  },
+  false
+);
+
+img.addEventListener(
+  "mouseout",
+  function (e) {
+    btns.classList.remove("btnsSlide");
+  },
+  false
+);
