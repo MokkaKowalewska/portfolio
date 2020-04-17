7eedbfa43ca70a83743eed0b250eedbb231bc116
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-17 17:38:58 +0200

    feat Responsive Navigation

---
+++ js/navigation.js
@@ -0,0 +1,12 @@
+(function getNav() {
+  const nav = document.querySelector(".menu__ul"),
+    burger = document.querySelector(".burger");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+
+  nav.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+})();
