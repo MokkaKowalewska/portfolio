405e2fd13ea80b64549b0140410a2a147e7bfb06
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-19 02:20:17 +0200

    minor: bio improving text and margins

--- contact-form.php
+++ contact-form.php
@@ -5,21 +5,19 @@ if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) 
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = nl2br($_POST["message"]);
-    $to = "web.monikakowalewska@gmail.com";
+    $to = "web . monikakowalewska @ gmail . com";
     $from = $email;
-    $subject = "MONIKAKOWALEWSKA.PL MESSAGE!";
+    $subject = "MONIKAKOWALEWSKAPL MESSAGE";
     $message = "<b>Name:</b>" . $name . "<br><b>Email:</b>" . $email . "<br><b>Message:</b>" . "<p>" . $message . "</p>";
     $headers = "From: $from\n";
     $headers .= "MIME-Version: 1.0\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\n";
 
-        mail($to, $subject, $message, $headers);
-
-        // ) {
-        //     echo "success";
-        // } else {
-        //     echo "Oopsie! The server failed to send the message. I'm sorry, could You try again later?";
-        // }
+        if(mail($to, $subject, $message, $headers))  {
+            echo "success";
+        } else {
+            echo "Oopsie! The server failed to send the message. I'm sorry, could You try again later?";
+        }
 
 
 }
--- js/send-form.js
+++ js/send-form.js
@@ -1,41 +1,38 @@
 const submitBtn = document.querySelector(".contact__btn");
+form = document.querySelector(".form");
 
-function submitForm() {
-  const form = document.querySelector(".form"),
+form.addEventListener(
+  "submit",
+  function submitForm() {
+    const status = document.querySelector(".contact__status"),
+      name = document.getElementById("name");
 
-    status = document.querySelector(".contact__waitMsg"),
-    name = document.getElementById("name");
+    submitBtn.disabled = true;
+    status.textContent = "sending...";
 
-  submitBtn.disabled = true;
-  status.textContent = "sending...";
+    console.log(name);
 
-  console.log(name);
+    let formdata = new FormData();
+    formdata.append("name", name.value);
+    formdata.append("email", document.getElementById("email").value);
+    formdata.append("message", document.getElementById("message").value);
 
-  let formdata = new FormData();
-  formdata.append("name", name.value);
-  formdata.append("email", document.getElementById("email").value);
-  formdata.append("message", document.getElementById("message").value);
+    console.log(document.querySelector("input #name").value);
 
-  console.log(document.querySelector("input #name").value);
-
-  let ajax = new XMLHttpRequest();
-  ajax.open("POST", "contact-form.php");
-  ajax.onreadystatechange = function () {
-    if (ajax.readyState == 4 && ajax.status == 200) {
-      if (ajax.responseText == "success") {
-        form.textContent(
-          "Thank You for You message," +
-            document.querySelector("input #name").value +
-            "!"
-        );
-      } else {
-        status.textContent = ajax.responseText;
-        submitBtn.disabled = false;
+    let ajax = new XMLHttpRequest();
+    ajax.open("POST", "contact-form.php");
+    ajax.onreadystatechange = function () {
+      if (ajax.readyState == 4 && ajax.status == 200) {
+        if (ajax.responseText == "success") {
+          form.innerHTML("Thank You for You message," + name.value + "!");
+        } else {
+          status.textContent = ajax.responseText;
+          submitBtn.disabled = false;
+        }
       }
-    }
-  };
+    };
 
-  ajax.send(formdata);
-}
-
-submitBtn.addEventListener(submitForm);
+    ajax.send(formdata);
+  },
+  false
+);
