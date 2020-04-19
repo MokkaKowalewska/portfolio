244d1fc75795da3e32420c4636d72e80b1ba0653
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-19 15:22:17 +0200

    feat: contact form working! PHP and JS done!

--- contact-form.php
+++ contact-form.php
@@ -1,19 +1,29 @@
 <?php
 
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
+ini_set("SMTP", "serwer1940016.home.pl ");
+ini_set("sendmail_from", "hello@monikakowalewska.pl");
+ini_set("smtp_port", "465");
+
 
 if(isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["message"])) {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = nl2br($_POST["message"]);
-    $to = "web . monikakowalewska @ gmail . com";
-    $from = $email;
+    $to = "hello@monikakowalewska.pl";
+    $from = "hello@monikakowalewska.pl";
     $subject = "MONIKAKOWALEWSKAPL MESSAGE";
-    $message = "<b>Name:</b>" . $name . "<br><b>Email:</b>" . $email . "<br><b>Message:</b>" . "<p>" . $message . "</p>";
+    $content = "<b>Name:</b>" . $name . "<br><b>Email:</b>" . $email . "<br><b>Message:</b>" . "<p>" . $message . "</p>";
     $headers = "From: $from\n";
     $headers .= "MIME-Version: 1.0\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\n";
 
-        if(mail($to, $subject, $message, $headers))  {
+    mail($to, $subject, $content, $headers);
+
+        if(mail($to, $subject, $content, $headers))  {
             echo "success";
         } else {
             echo "Oopsie! The server failed to send the message. I'm sorry, could You try again later?";
--- js/send-form.js
+++ js/send-form.js
@@ -1,34 +1,36 @@
-const submitBtn = document.querySelector(".contact__btn");
-form = document.querySelector(".form");
-
 function submitForm() {
   const status = document.querySelector(".contact__status"),
+    submitBtn = document.querySelector(".contact__btn"),
+    form = document.querySelector(".form"),
     name = document.getElementById("name");
 
   submitBtn.disabled = true;
   status.innerHTML = "sending...";
 
-  console.log(name);
-
-  let formdata = new FormData();
+  var formdata = new FormData();
   formdata.append("name", name.value);
   formdata.append("email", document.getElementById("email").value);
   formdata.append("message", document.getElementById("message").value);
 
-  console.log(document.querySelector("input #name").value);
+  var ajax = new XMLHttpRequest();
 
-  let ajax = new XMLHttpRequest();
-  ajax.open("POST", "../contact-form.php");
+  ajax.open("POST", "contact-form.php", true);
+  console.log("nanana" + ajax);
+  ajax.send(formdata);
+
   ajax.onreadystatechange = function () {
+    console.log(ajax);
     if (ajax.readyState == 4 && ajax.status == 200) {
       if (ajax.responseText == "success") {
-        form.innerHTML = "Thank You for You message," + name.value + "!";
+        //
+        status.innerHTML = ajax.responseText;
       } else {
         status.innerHTML = ajax.responseText;
         submitBtn.disabled = false;
       }
     }
   };
-
-  ajax.send(formdata);
+  console.log(ajax.readyState);
+  // ajax.setRequestHeader("blabla", "blablabla");
+  console.log(ajax.readyState);
 }
