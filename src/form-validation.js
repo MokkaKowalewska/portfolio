3368acb1cdf476029d50518f445190e577944eee
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-25 11:14:21 +0200

    refactor: extract event listener from realtimeValidation to eventBlur

--- src/form-validation.js
+++ src/form-validation.js
@@ -9,7 +9,7 @@ export default class ValidateForm {
     this.errorMsg = document.querySelectorAll(errorMsgClassName);
     this.possibleVioletions = ["valueMissing", "patternMismatch", "tooShort"];
     this.messages = {
-      valueMissing: "Yikes, this field cannot be empty!",
+      valueMissing: "Oh noes, this field cannot be empty!",
       patternMismatch: "Enter valid email, pretty please",
       tooShort: "Please write at least 3 signs, 'Hey' will do! :)",
       check: "Check!",
@@ -24,40 +24,38 @@ export default class ValidateForm {
   }
 
 
-  realtimeValidation() {
-    this.inputs.forEach((input) => {
-      input.addEventListener(
-        "blur", debounce((event) => {
-          let testedInput = event.target;
-          let { validity } = testedInput;
-          let errorMsgColor;
+  realtimeValidation(event) {
+    let testedInput = event.target;
+    let { validity } = testedInput;
 
-          // eslint-disable-next-line no-restricted-syntax
-          for (let violetion in validity) {
-            if (validity[violetion] === true && violetion !== "valid") {
-              console.log(`${validity[violetion]}is validity violetion`);
-              console.log(`${violetion}is violetion`);
-              console.log(typeof violetion);
-              this.displayErrors(testedInput, violetion);
-              testedInput.nextElementSibling.style.webkitTextFillColor = "#ff2424";
-              return;
-            }
-            this.displayErrors(testedInput, "check");
-            testedInput.nextElementSibling.style.webkitTextFillColor = "#5eb15e";
-            console.log(`${validity[violetion]}is validity violetion`);
-            console.log(`${violetion}is violetion`);
-            console.log("cheeck");
-          }
-        }),
-      );
-    }, false);
+    // eslint-disable-next-line no-restricted-syntax
+    for (let violetion in validity) {
+      if (validity[violetion] === true && violetion !== "valid") {
+        console.log(`${validity[violetion]}is validity violetion`);
+        console.log(`${violetion}is violetion`);
+        console.log(typeof violetion);
+        this.displayErrors(testedInput, violetion);
+        testedInput.nextElementSibling.style.webkitTextFillColor = "#ff2424";
+        return;
+      }
+      this.displayErrors(testedInput, "check");
+      testedInput.nextElementSibling.style.webkitTextFillColor = "#5eb15e";
+      console.log(`${validity[violetion]}is validity violetion`);
+      console.log(`${violetion}is violetion`);
+      console.log("cheeck");
+    }
   }
 
   displayErrors(inputValidated, violetion) {
     inputValidated.nextElementSibling.textContent = this.messages[violetion];
     inputValidated.setAttribute("aria-describedby", `error-for-${inputValidated.id}`);
-	 }
+  }
+
+  eventBlur(e) {
+    this.inputs.forEach((input) => {
+      input.addEventListener(
+        "blur", debounce(realtimeValidation, 300), false,
+      );
+    });
+  }
 }
-
-
-//
