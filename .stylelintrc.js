46a6e7f450aea3a65aee9450e73e74f3612e30cc
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-04-30 23:52:09 +0200

    style: cleaning SCSS, changing order of rules, refactor

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": "stylelint-config-sass-guidelines",
   "rules": {
     "max-nesting-depth": 2,
-    "string-quotes": "double"
+    "string-quotes": "double",
+    "properties-alphabetical-order": "off"
   }
 }
